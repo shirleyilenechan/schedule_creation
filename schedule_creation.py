c9693bb0c6845c371d1d7048442b5160b9eeb060
shirleyilenechan <schan1@pagerduty.com>
2023-06-04 18:18:53 -0700

    add dataframe logic

--- schedule_creation.py
+++ schedule_creation.py
@@ -1,13 +1,22 @@
 import requests
 import os
 from icalendar import Calendar
-import datetime
+from datetime import datetime as dt, timedelta, timezone
+import pytz
+from dateutil.rrule import rrulestr
+import pandas as pd
+import argparse
+import sys
+
 
 def import_calendar(calendar):
     if calendar.startswith('http'):
-        response = requests.get(calendar)
-        response.raise_for_status()
-        calendar_data = response.text
+        try: 
+            response = requests.get(calendar)
+            calendar_data = response.text
+        except requests.exceptions.RequestException:
+            raise SystemExit("Cannot import calendar from invalid URL")
+            
     else:
         try:
             with open(calendar, 'rb') as f:
@@ -18,59 +27,87 @@ def import_calendar(calendar):
     calendar = Calendar.from_ical(calendar_data)
     return calendar
 
-def get_user(user_id, api_key):
-    api_url = f"https://api.pagerduty.com/users/{user_id}"
+def get_user(pd_user_id, api_key):
+    api_url = f"https://api.pagerduty.com/users/{pd_user_id}"
     headers = {
         "Content-Type": "application/json",
         "Accept": "application/vnd.pagerduty+json;version=2",
-        "Authorization": api_key 
+        "Authorization": f"Token token={api_key}"
     }
 
-    response = requests.get(api_url, headers=headers)
-    response.raise_for_status()
-    user = response.json()
+    try: 
+        response = requests.get(api_url, headers=headers)
+        user = response.json()
+    except requests.exceptions.RequestException:
+        raise SystemExit(f"Error getting user from PagerDuty'{pd_user_id}'")
 
     return user
 
-def create_schedule_layers(calendar, api_key):
-    schedule_layers = {}
-    i = 1
-
+def create_schedule_df(calendar, api_key):
+    df = pd.DataFrame(columns = ["name", "start" "end"])
+    shifts = []
+    
     for component in calendar.walk():
+        pd_user_id = component.get('summary')
+
         if component.name == 'VEVENT':
-            pd_user_id = component.get('SUMMARY', '')
-            event_date = component.get('DTSTART').dt.date()
-            event_start_time = component.get('DTSTART').dt.time()
-            event_end_time = component.get('DTEND').dt.time()
-            weekday_number = event_date.isoweekday()
-
-            try:
+            if pd_user_id:
                 user = get_user(pd_user_id, api_key)
+            else:
+                raise("User ID could not be extracted from event summary")
+            
+            start_dt = component.get('dtstart').dt
+            end_dt = component.get('dtend').dt
+            
+            if component.get("rrule"):
+                rrule = component.get("rrule")
+                instances = extract_recurring_events(pd_user_id, rrule, start_dt, end_dt)
+                shifts.extend(instances)
 
-                start = datetime.combine(event_date, event_start_time).strftime("%Y-%m-%dT%H:%M")
-                rotation_virtual_start = start
-
-                start_datetime = datetime.combine(event_date, event_start_time)
-                end_datetime = datetime.combine(event_date, event_end_time)
-                rotation_turn_length_seconds = (end_datetime - start_datetime).total_seconds()
-
-                users = [
-                    {
-                        'user_id': pd_user_id,
-                        'type': 'user_reference'
-                    }
-                ]
-
-                layer = {
-                    'start': start,
-                    'rotation_virtual_start': rotation_virtual_start,
-                    'rotation_turn_length_seconds': rotation_turn_length_seconds,
-                    'users': users
+            else:                 
+                shift = {
+                    "name": pd_user_id,
+                    "start": start_dt,
+                    "end": end_dt,
+                    "recurring": False
                 }
 
-                layer_name = f'layer_{i}'
-                schedule_layers[layer_name] = layer
-            except requests.exceptions.RequestException:
-                raise RequestException(f"Error getting user from PagerDuty'{pd_user_id}'")
+                shifts.append(shift)
 
-    return schedule_layers
+    df = pd.DataFrame(shifts)
+    return df
+
+                
+def extract_recurring_events(name, rrule, start, end):
+
+    instances = []
+    rrule_str = rrule.to_ical().decode("utf-8")
+    rule = rrulestr(rrule_str)
+    
+    shift_length = end - start
+
+    shifts = list(rule)
+
+    for shift_start in shifts:
+        shift = {
+            "name": name,
+            "start":shift_start,
+            "end": shift_start + shift_length,
+            "recurring": True
+        }
+
+        instances.append(shift)
+
+    return instances
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--calendar", "-c", dest="calendar", required=True)
+    parser.add_argument("--api_key", "-a", dest="api_key", required=True)
+    args = parser.parse_args()
+    calendar = import_calendar(args.calendar)
+    create_schedule_df(calendar, args.api_key)
+
+if __name__ == '__main__':
+    sys.exit(main())
+
